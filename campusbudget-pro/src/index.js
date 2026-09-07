73ae9537da83cd482b686304169a342a9bdf74fc
diabolicalG <odexrena@gmail.com>
2026-09-07 03:39:07 +0300

    Update index.js to include Capacitor initialization

--- campusbudget-pro/src/index.js
+++ campusbudget-pro/src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Capacitor } from '@capacitor/core';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+// Check if running on mobile platform
+const isMobile = Capacitor.isNativePlatform();
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Log platform info
+console.log('Running on platform:', Capacitor.getPlatform());
+console.log('Is native platform:', isMobile);
